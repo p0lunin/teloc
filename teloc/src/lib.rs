9f7d22a5390c4d4709d10c253834b31054c4246b
p0lunin <dmytro.polunin@gmail.com>
2020-11-08 19:23:43 +0200

    added documentation for lib.rs module

--- teloc/src/lib.rs
+++ teloc/src/lib.rs
@@ -1,3 +1,57 @@
+//! There are 2 types can be provider of services: `ServiceProvider` and `Scope`. First used as store for dependencies with
+//! `Instance` and `Singleton` lifetimes, and for declaring all dependencies using `.add_*()` methods. `Scope` can be
+//! created from `ServiceProvider` object by calling method `ServiceProvider::scope`.
+//!
+//! There are four lifetimes for dependencies:
+//! 1. `Transient`. Service will be created when resolves. Can depend on dependencies with anything lifetime. If depend on
+//! dependency with `Scoped` lifetime can be resolves only from `Scope` object.
+//! 2. `Scoped`. Service will be created once at `Scope` when it resolved (lazy). Can depend on dependencies with anything
+//! lifetime.
+//! 3. `Singleton`. Service will be created once at `ServiceProvider` when it resolved (lazy). Can depend on dependencies
+//! with anything lifetime exclude `Scoped`.
+//! 4. `Instance`. Dependency was created outside of `ServiceProvider`.
+//!
+//! Process of working with library:
+//! 1. Define your structs.
+//! 2. Create constructors and add `#[inject]` macro on its.
+//! 3. Create a `ServiceProvider` object.
+//! 4. Add your services and dependencies using `ServiceProvider::add_*` methods.
+//! 5. Create `Scope` if need.
+//! 6. Get service from container using `.resolve()` method.
+//! 7. Work with service.
+//!
+//! Example:
+//! ```rust
+//! use teloc::{inject, Resolver, ServiceProvider, Dependency};
+//!
+//! struct ConstService {
+//!     number: i32,
+//! }
+//! #[inject]
+//! impl ConstService {
+//!     pub fn new(number: i32) -> Self {
+//!         ConstService { number }
+//!     }
+//! }
+//!
+//! // derive macro can be used when all fields implement `Dependency` trait, but we do not recommend use it in production
+//! // code
+//! #[derive(Dependency)]
+//! struct Controller {
+//!     number_service: ConstService,
+//! }
+//!
+//! fn main() {
+//!     let container = ServiceProvider::new()
+//!         .add_scoped_i::<i32>()
+//!         .add_transient::<ConstService>()
+//!         .add_transient::<Controller>();
+//!     let scope = container.scope(teloc::scopei![10]);
+//!     let controller: Controller = scope.resolve();
+//!     assert_eq!(controller.number_service.number, 10);
+//! }
+//! ```
+
 #![forbid(unsafe_code)]
 
 pub mod container;
