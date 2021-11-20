e2fffc331e7741c3f574de2be7d34cf63c667da8
Adam Burkhalter <aburkhalter512@gmail.com>
2021-11-19 23:57:21 -0800

    Add unit tests to ensure lifetimes are preserved.

--- teloc/tests/lifetimes.rs
+++ teloc/tests/lifetimes.rs
@@ -58,3 +58,105 @@ fn test_lifetimes() {
     assert_eq!(si1.u, si2.u);
     assert_eq!(i1.u, i2.u);
 }
+
+#[test]
+fn test_resolve_nested_fork() {
+    let provider = ServiceProvider::new().add_instance(10u8);
+    let forked_provider = provider.fork();
+    let double_forked_provider = forked_provider.fork();
+
+    let num: &u8 = double_forked_provider.resolve();
+    assert_eq!(num, &10u8);
+}
+
+#[test]
+fn test_forked_lifetime() {
+    let provider = ServiceProvider::new().add_instance(10u8);
+    let num: &u8 = provider.resolve();
+
+    let forked_num: &u8 = {
+        let provider = provider.fork();
+        provider.resolve()
+    };
+
+    assert_eq!(num, forked_num);
+}
+
+#[test]
+fn test_resolve_instance_with_greater_lifetime() {
+    let num = 10u8;
+
+    let resolved_num: &u8 = {
+        let provider = ServiceProvider::new().add_instance(&num);
+        provider.resolve()
+    };
+
+    assert_eq!(num, *resolved_num);
+}
+
+#[test]
+fn test_resolve_singleton_with_greater_lifetime() {
+    let provider = ServiceProvider::new().add_singleton::<Uuid>();
+    let uuid: &Uuid = provider.resolve();
+
+    let forked_uuid: &Uuid = {
+        let provider = provider.fork();
+        provider.resolve()
+    };
+
+    assert_eq!(uuid, forked_uuid);
+}
+
+#[test]
+fn test_resolve_transient_with_greater_lifetime() {
+    let provider = ServiceProvider::new().add_transient::<Uuid>();
+    let uuid: Uuid = provider.resolve();
+
+    let forked_uuid: Uuid = {
+        let provider = provider.fork();
+        provider.resolve()
+    };
+
+    assert_ne!(uuid, forked_uuid);
+}
+
+struct SingletonWithBorrowedDep<'a> {
+    dep: &'a i32,
+}
+
+#[inject]
+impl<'a> SingletonWithBorrowedDep<'a> {
+    pub fn new(dep: &'a i32) -> Self {
+        Self { dep }
+    }
+}
+
+#[test]
+fn test_resolve_singleton_deps_with_greater_lifetime() {
+    let provider = ServiceProvider::new()
+        .add_instance(10i32)
+        .add_singleton::<SingletonWithBorrowedDep>();
+    let singleton: &SingletonWithBorrowedDep = provider.resolve();
+
+    let forked_singleton: &SingletonWithBorrowedDep = {
+        let provider = provider.fork();
+        provider.resolve()
+    };
+
+    assert_eq!(singleton.dep, forked_singleton.dep);
+}
+
+#[test]
+fn test_resolve_transient_deps_with_greater_lifetime() {
+    let provider = ServiceProvider::new()
+        .add_instance(10i32)
+        .add_transient::<SingletonWithBorrowedDep>();
+    let singleton: SingletonWithBorrowedDep = provider.resolve();
+
+    let forked_singleton: SingletonWithBorrowedDep = {
+        let provider = provider.fork();
+        provider.resolve()
+    };
+
+    assert_eq!(singleton.dep, forked_singleton.dep);
+}
